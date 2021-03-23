e139e916492d650428ed52e587e78cbe58514414
buixankhai1204 <khaibuixuan2@gmail.com>
2021-03-23 17:55:10 +0700

    second commit

--- composetest/site/index.php
+++ composetest/site/index.php
@@ -13,25 +13,24 @@ if (!$resp) {
 curl_close($curl);
 //-----------------------------------------------------------------------------------
 try {
+  $resp = mb_convert_encoding($resp, 'HTML-ENTITIES', "UTF-8");
   $dom = new DOMDocument();
-  if (!@$dom->loadHtml($reasp)) {
+  if (!@$dom->loadHtml($resp)) {
     throw new Exception('khong the load duoc HTML');
   }
   $xpath = new DOMXPath($dom);
 
-  $xPahtName = $xpath->query('//div[@class="rightBox-3"]/div/div/h4');
-  $xPahtThumb = $xpath->query('//div[@class="rightBox-3"]/div/div/div/a/img/@data-src');
+  $xPahtName = $xpath->query('//div[@class="timeline loadmore"]/div[@class="story" and position()<=7]/h4[@class="story__heading"]/a');
+  $xPahtThumb = $xpath->query('//div[@class="timeline loadmore"]/div[@class="story" and position()<=7]/div[@class="story__thumb"]/a/img/@data-src');
 
-  if ($xPahtName == "" || $xPahtThumb == "") {
-    throw new Exception('du lieu khong ton tai');
+  if (count($xPahtName) != count($xPahtThumb)) {
+    json_encode('co bai viet bi loi');
   }
   $title = [];
   $thumb = [];
-  foreach ($xPahtName as $item) {
-    array_push($title, $item->textContent);
-  }
-  foreach ($xPahtThumb as $item) {
-    array_push($thumb, $item->textContent);
+  foreach ($xPahtName as $index => $xPahtName) {
+    array_push($title, $xPahtName->textContent);
+    array_push($thumb, $xPahtThumb[$index]->textContent);
   }
   $result = array_combine($title, $thumb);
 } catch (Exception $e) {
@@ -46,14 +45,10 @@ try {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css">
   <title>Document</title>
 </head>
 <style>
-  .container {
-    width: 1400px;
-    margin: 0px auto;
-  }
-
   ul {
     display: flex;
     justify-content: space-between;
@@ -67,7 +62,8 @@ try {
 </style>
 
 <body>
-  <div class="container">
+
+  <div class="wrapper">
     <ul>
       <?php foreach ($result as $index => $value) { ?>
         <li>
@@ -75,8 +71,36 @@ try {
         </li>
       <?php } ?>
     </ul>
+    <div class="form-input">
+      <label for="input4" class="icon glyphicon glyphicon-pencil"></label>
+      <button id="convert" class="button glyphicon glyphicon-search"></button>
+      <input id="uid" name="titlePost" type="text" class="input">
+    </div>
+    <div class="form-input">
+      <input id="guid" type="text" class="input" placeholder="" readonly />
+    </div>
   </div>
 
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+  $("#convert").click(function() {
+    var nameItem = $("#uid").val();
+    var data = {
+      nameItem: nameItem
+    }
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8081/searchXpath.php",
+      data: data,
+      dataType: "text",
+      success: function(result) {
+        if (result['status_code'] == 200) {
+        
+        }
+      }
+    });
+  });
+</script>
 
 </html>
---
+++ composetest/site/searchXpath.php
@@ -0,0 +1,56 @@
+<?php
+
+try {
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://baomoi.com/chu-de.epi',
+    CURLOPT_SSL_VERIFYPEER => false,
+  ));
+  $resp = curl_exec($curl);
+  if (!$resp) {
+    echo "<p>cURL error number: " . curl_errno($c) . " on URL: " . $url . "</p>" .
+      "<p>cURL error: " . curl_error($c) . "</p>";
+  }
+  curl_close($curl);
+  $resp = mb_convert_encoding($resp, 'HTML-ENTITIES', "UTF-8");
+ 
+  $dom = new DOMDocument();
+  if (!@$dom->loadHtml($resp)) {
+    throw new Exception('khong the load duoc HTML');
+  }
+  $xpath = new DOMXPath($dom);
+  $inputTitle = $_POST['nameItem'];
+  $StrContent = $dom->textContent;
+  $title = [];
+  $thumb = [];
+  $getCategory;
+  if (strpos($dom->textContent, $inputTitle)) {
+    $xPathSearchParent = $xpath->query("//*[contains(text(),'$inputTitle')]/parent::*/parent::*/parent::*/parent::*/@data-zone");
+    foreach ($xPathSearchParent as $item) {
+      $getAid = $item->textContent;
+    }
+    $xPathNameCategory = $xpath->query("//div[@class='loadmore']/div[contains(@data-zone,'$getAid')]/div[1]");
+    $getCategory = $xPathNameCategory[0]->textContent;
+    $xPathTitle = $xpath->query("//div[@class='loadmore']/div[contains(@data-zone,'$getAid')]/div/div/h4/a");
+    $xPathThumb = $xpath->query("//div[@class='loadmore']/div[contains(@data-zone,'$getAid')]/div[2]/div/div[1]/a/img/@src|//div[@class='loadmore']/div[contains(@data-zone,'$getAid')]/div[2]/div/div[1]/a/img/@data-src");
+    foreach ($xPathTitle as $index => $item) {
+      array_push($title, $item->textContent);
+      array_push($thumb, $xPathThumb[$index]->textContent);
+    }
+    $result = array_combine($title, $thumb);
+  }
+  $array_respone = array(
+    "success" => true,
+    "status_code" => 200,
+    "data" => $result,
+    "message" => "success",
+    "error" => "",
+  );
+  
+  echo '<pre>';
+  print_r($title);
+  echo '</pre>';
+  echo json_encode($array_respone);
+} catch (Exception $e) {
+}
